9524c45723f9bf8721a4961ee296be302b09d12f
linsec <li0966663305@gmail.com>
2026-09-21 22:52:27 +0800

    test

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import plotly
+
+print("Streamlit:", streamlit.__version__)
+print("yfinance:", yf.__version__)
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+print("Plotly:", plotly.__version__)
+
+prㄕㄛint("\n所有套件載入成功！")
